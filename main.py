4644827ca2262fe1e3a48fec4fcc4a9140939612
KOTNI KANAKA RAJU <83851187+kotnikanakaraju@users.noreply.github.com>
2026-04-20 13:21:13 +0530

    Update DATABASE_URL to use Docker service name

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import sessionmaker, declarative_base, Session
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/mydb"
+DATABASE_URL = "postgresql://postgres:postgres@db:5432/mydb"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
